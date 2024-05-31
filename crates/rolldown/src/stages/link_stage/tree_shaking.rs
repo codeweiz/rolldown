e0431807dbe5299da3de21834dbf8f9d69f19659
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-06-01 00:08:42 +0800

    fix: 🐛 better perf

--- crates/rolldown/src/stages/link_stage/tree_shaking.rs
+++ crates/rolldown/src/stages/link_stage/tree_shaking.rs
@@ -29,15 +29,15 @@ fn include_module(ctx: &mut Context, module: &NormalModule) {
     return;
   }
 
-  if ctx.tree_shaking {
-    let forced_no_treeshake = matches!(module.side_effects, DeterminedSideEffects::NoTreeshake);
+  let forced_no_treeshake = matches!(module.side_effects, DeterminedSideEffects::NoTreeshake);
+  if ctx.tree_shaking && !forced_no_treeshake {
     module.stmt_infos.iter_enumerated().for_each(|(stmt_info_id, stmt_info)| {
       // Skip the first statement, which is the namespace object. It should included only if it is used no matter
       // tree shaking is enabled or not.
       if stmt_info_id.index() == 0 {
         return;
       }
-      if stmt_info.side_effect && !forced_no_treeshake {
+      if stmt_info.side_effect {
         include_statement(ctx, module, stmt_info_id);
       }
     });
